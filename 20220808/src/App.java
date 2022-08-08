3ce8196b9d7192456c628d1a8605b5059073c025
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-08 10:01:19 +0900

    feat : Number outfut
    테스트 2

--- 20220808/src/App.java
+++ 20220808/src/App.java
@@ -6,5 +6,15 @@ public class App {
         String value = scanner.nextLine();
 
         System.out.println("입력된 정수는 " + value + "입니다.");
+
+        int[] Array = new int[] { 345, 567, 789 };
+        int value1 = 0;
+        for (int i = 0; i < Array.length - 1; i++) {
+            if (Array[i] < Array[i + 1]) {
+                value1 = Array[i + 1];
+            }
+        }
+        System.out.println(value1);
+        scanner.close();
     }
 }
