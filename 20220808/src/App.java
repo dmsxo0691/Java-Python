bf20360feec1fc64e420dde37826894de9e38fae
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-08 09:47:53 +0900

    feat : Add scanner
    신임 교수님 교육생 테스트

--- 20220808/src/App.java
+++ 20220808/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+
+        System.out.println("입력된 정수는 " + value + "입니다.");
     }
 }
