675d3fa4a6cda4162aadb50864c3a0003521e55f
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-09 10:08:19 +0900

    feat : Cat Dog
    추상 클래스 연습

---
+++ 20220809/App1/src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Animal {
+
+    @Override
+    void crying() {
+        // TODO Auto-generated method stub
+        System.out.println("야옹");
+    }
+
+}
---
+++ 20220809/App1/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal {
+
+    @Override
+    void crying() {
+        // TODO Auto-generated method stub
+        System.out.println("멍멍");
+    }
+
+}
