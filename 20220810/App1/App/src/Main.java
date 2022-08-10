adf59327d06ee1ce9c3fb6e959278b6e21b33545
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 12:39:04 +0900

    feat : insert
    문자열 삽입

--- 20220810/App1/App/src/Main.java
+++ 20220810/App1/App/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuffer str = new StringBuffer("Java Oracle");
+        StringBuffer str = new StringBuffer("Java 만세!!");
         System.out.println("원본 문자열 : " + str);
 
-        System.out.println(str.delete(4, 8));
-        System.out.println(str.deleteCharAt(1));
-        System.out.println("deleteCharAt()메서드 호출 후 원본 문자열 : " + str);
+        System.out.println(str.insert(4, "Script"));
+        System.out.println(str);
+
     }
 }
