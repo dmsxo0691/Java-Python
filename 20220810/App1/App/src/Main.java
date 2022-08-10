e57defa3fef54ffa33a648927f73a64b32bca3e9
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 10:54:09 +0900

    feat : Fanal
    Fanal 사용법, 함수에 fanal 적용

---
+++ 20220810/App1/App/src/Main.java
@@ -0,0 +1,11 @@
+public class Main extends Parent {
+    public void show() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.show();
+    }
+}
---
+++ 20220810/App1/App/src/Parent.java
@@ -0,0 +1,5 @@
+public class Parent {
+    public void show() {
+        System.out.println("hi");
+    }
+}
