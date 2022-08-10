3cba5c029ddc65ea3cf11908957ad48293be05cc
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 11:29:35 +0900

    feat : Trim
    공백 삭제

--- 20220810/App1/App/src/Main.java
+++ 20220810/App1/App/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String str = new String("Java");
+        String str = new String("   Java        ");
         System.out.println("원본 문자열: " + str);
+        System.out.println(str + "|");
+        System.out.println(str.trim() + "|");
 
-        System.out.println(str.concat("수업"));
-        System.out.println("concat() 메서드 호출 후 원본 문자열: " + str);
     }
 }
