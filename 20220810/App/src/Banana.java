7b6d25754bb0defdc60d4efda85c02d80e2001e0
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 09:31:25 +0900

    feat : Polymorphism
    다형성 도입

---
+++ 20220810/App/src/Banana.java
@@ -0,0 +1,8 @@
+public class Banana extends Fruit {
+
+    public Banana() {
+        price = 1000;
+        name = "바나나";
+        fresh = 80;
+    }
+}
---
+++ 20220810/App/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Fruit fruit = new Peach();
+        fruit.show();
+    }
+}
