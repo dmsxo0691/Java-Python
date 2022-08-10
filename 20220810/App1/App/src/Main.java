6b1623006d8de6c3bcc7777dc624f882aa98affa
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 11:16:41 +0900

    feat : CharAt()
    문자열에서 문자 추출

--- 20220810/App1/App/src/Main.java
+++ 20220810/App1/App/src/Main.java
@@ -1,11 +1,12 @@
-public class Main extends Parent {
-    public void show() {
-        System.out.println("hello");
-    }
-
+public class Main {
     public static void main(String[] args) {
+        String str = new String("Java");
+        System.out.println("원본 문자열 : " + str);
+        System.out.println("문자열 : " + str.charAt(1));
 
-        Main main = new Main();
-        main.show();
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println("\n charAt() 메소드 호출 후 원본 문자열: " + str);
     }
 }
--- 20220810/App1/App/src/Parent.java
+++
@@ -1,5 +0,0 @@
-public class Parent {
-    public void show() {
-        System.out.println("hi");
-    }
-}
