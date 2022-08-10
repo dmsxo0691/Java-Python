fb9c0a12057c9fb4759030148969fd90ed9f74b4
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-10 11:19:39 +0900

    feat : concat
    문자열에 문자열 추가

--- 20220810/App1/App/src/Main.java
+++ 20220810/App1/App/src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String str = new String("Java");
-        System.out.println("원본 문자열 : " + str);
-        System.out.println("문자열 : " + str.charAt(1));
+        System.out.println("원본 문자열: " + str);
 
-        for (int i = 0; i < str.length(); i++) {
-            System.out.print(str.charAt(i) + " ");
-        }
-        System.out.println("\n charAt() 메소드 호출 후 원본 문자열: " + str);
+        System.out.println(str.concat("수업"));
+        System.out.println("concat() 메서드 호출 후 원본 문자열: " + str);
     }
 }
