3fc9810f59b3828a2a8b24d2ca4ccc86c8ac39ed
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-16 09:29:10 +0900

    feat : <>
    제너릭에 관한 설명

--- 20220816/App/src/prog.java
+++ 20220816/App/src/prog.java
@@ -3,17 +3,15 @@ import java.util.ArrayList;
 public class prog {
     public static void main(String[] args) {
 
-        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        ArrayList arrList = new ArrayList();
+        // <Integer> 제네릭
 
-        arrList.add(40);
-        arrList.add(20);
-        arrList.add(30);
-        arrList.add(10);
+        arrList.add("hello world");
 
-        for (int i = 0; i < arrList.size(); i++) {
-            System.out.println(arrList.get(i) + " ");
-        }
-        System.out.println();
+        String myString = (String) arrList.get(0);
+        System.out.println(myString);
 
     }
+    // 자바에서 제네릭이란 데이터의 타입을 일반화한다.
+    // 제네릭은 클래스나 메소드에서 사용할 내부 데이터 타입을 컴파일 시에 미리 지정하는 방법이다.
 }
