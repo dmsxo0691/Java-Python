d4c8dfd64ec947b4f2872964bb5a979d73717f97
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-16 10:41:05 +0900

    feat : add String
    주석 추가

--- 20220816/App/src/stack.java
+++ 20220816/App/src/stack.java
@@ -6,6 +6,7 @@ public class stack {
 
         Stack<Integer> st = new Stack<Integer>(); // 스택의 생성
         // Deque<Integer> st = new ArrayDeque<Integer>();
+        // LIFO - 라스트 인 퍼스트 아웃
 
         // push() 메소들르 이용한 요소의 저장
         st.push(4);
@@ -15,6 +16,12 @@ public class stack {
 
         // peek() 메소드를 이용한 요소의 반환
         System.out.println(st.peek());
+        // 제일 마지막으로 저장된 요소 반환(제일 상단)
         System.out.println(st);
+
+        System.out.println(st.pop());
+        // 제일 마지막으로 저장된 요소 삭제(제일 상단)
+        System.out.println(st);
+
     }
 }
