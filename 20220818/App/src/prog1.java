7f2f5f83177cf9c194eccc6f638bd0baf11caadf
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-18 09:45:43 +0900

    feat : Key

--- 20220818/App/src/prog1.java
+++ 20220818/App/src/prog1.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class prog1 {
     public static void main(String[] args) {
@@ -16,6 +17,28 @@ public class prog1 {
         for (String key : hm.keySet()) {
             System.out.println(String.format("키 : %s, 값: %s", key, hm.get(key)));
         }
+
+        hm.remove("사십");
+        System.out.println();
+
+        // iterator() 메소드를 이용한 요소출력
+        Iterator<String> keys = hm.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            System.out.println(String.format("키 : %s, 값: %s", key, hm.get(key)));
+        }
+
+        System.out.println();
+        hm.replace("이십", 200);
+
+        for (String key : hm.keySet()) {
+            System.out.println(String.format("키 : %s, 값: %s", key, hm.get(key)));
+        }
+
+        System.out.println();
+
+        // size() 메소드를 이용한 요소의 총 개수
+        System.out.println("맵의 크기 : " + hm.size());
     }
 }
 // Map 컬렉션 클래스들은 키와 값을 하나의 쌍으로 저장하는 방식을 사용
