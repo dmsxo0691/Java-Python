0e5646ffc601f37299690fefcfc7aa822421ea00
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-19 09:35:58 +0900

    feat: Get/Set

--- 20220819/App/src/MyPatentTest.java
+++ 20220819/App/src/MyPatentTest.java
@@ -19,7 +19,7 @@ public class MyPatentTest {
 
         MyParent p = new MyParent();
 
-        System.out.println(p.prv); // 에러
+        // System.out.println(p.prv); // 에러
         System.out.println(p.dft);
         System.out.println(p.prt);
         System.out.println(p.pub);
---
+++ 20220819/App/src/TimeTest.java
@@ -0,0 +1,41 @@
+class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        if (hour < 0 || hour > 23)
+            return; // 유효성 검사
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+}
+
+public class TimeTest {
+    public static void main(String[] args) {
+        Time t = new Time();
+        t.setHour(21);
+        System.out.println(t.getHour());
+
+    }
+}
