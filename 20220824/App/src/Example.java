9407638dafcc3867f53fff53146dd9005a94b12f
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-24 09:35:16 +0900

    faet : Example

---
+++ 20220824/App/src/Example.java
@@ -0,0 +1,25 @@
+public class Example {
+    interface Math {
+
+        int sum(int n1, int n2);
+
+    }
+
+    // Math 인터페이스를 구현한 MyMath 클래스의 객체가 한번만 사용되고
+    // 재사용되지 않는다면 비효율적일 수 있다 - 이럴때 익명함수 사용
+    static class MyMath implements Math {
+        public int sum(int n1, int n2) {
+            return n1 + n2;
+        }
+    }
+
+    static int doSomething(Math math) {
+        return math.sum(10, 20);
+    }
+
+    public static void main(String[] args) {
+        Math math = new MyMath();
+        int result = doSomething(math);
+        System.out.println(result);
+    }
+}
