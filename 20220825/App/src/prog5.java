b147f2934183d3ff802ee53e98119e1e450b5d51
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-25 09:37:26 +0900

    feat : Stream4

--- 20220825/App/src/prog5.java
+++ 20220825/App/src/prog5.java
@@ -1,3 +1,12 @@
-public class prog5 {
+import java.util.Random;
+import java.util.stream.IntStream;
 
+import javax.sound.sampled.SourceDataLine;
+
+public class prog5 {
+    public static void main(String[] args) {
+        // 특정 타입 난수로 이루어진 스트림 생성
+        IntStream stream = new Random().ints(4);
+        stream.forEach(System.out::println);
+    }
 }
