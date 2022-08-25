d043e694a86b91ad9d54db2a9cfac8c295b405af
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-25 11:43:09 +0900

    feat : Slide2

--- 20220825/slide1/js/main.js
+++ 20220825/slide1/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
-  $(".img_slide li").css("left", "100%");
-  $(".img_slide li").eq(0).css("left", 0);
+  $(".img_slide li").css("top", "100%");
+  $(".img_slide li").eq(0).css("top", 0);
   //첫번째 사진만 보이는 위치 eq(=equal) 0 (인덱스)
   var i = 0;
   setInterval(auto, 3000);
@@ -9,10 +9,10 @@ $(function () {
     if (i == 3) {
       i = 0;
     }
-    $(".img_slide li").eq(i).css("left", "100%").animate({ left: 0 }, 600);
+    $(".img_slide li").eq(i).css("top", "100%").animate({ top: 0 }, 600);
     $(".img_slide li")
       .eq(i - 1)
-      .css("left", "0")
-      .animate({ left: "-100%" }, 600);
+      .css("top", "0")
+      .animate({ top: "-100%" }, 600);
   }
 });
