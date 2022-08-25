a37329194410852217c05d25f33df7722a45502f
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-08-25 09:44:40 +0900

    feat : Stream5

--- 20220825/App/src/prog5.java
+++ 20220825/App/src/prog5.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.sound.sampled.SourceDataLine;
 
@@ -8,5 +9,12 @@ public class prog5 {
         // 특정 타입 난수로 이루어진 스트림 생성
         IntStream stream = new Random().ints(4);
         stream.forEach(System.out::println);
+
+        // iterate() 메소드를 이용한 무한 스트림
+        // Stream<Integer> stream1 = Stream.iterate(2, n -> n + 2);
+        // stream1.forEach(System.out::println);
+
+        Stream<Integer> stream2 = Stream.iterate(100, n -> n + 10).limit(5);
+        stream2.forEach(System.out::println);
     }
 }
