8ed1dc5784f61fecb3b7ca3c47fbd24813fd90ea
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-09-02 14:09:25 +0900

    feat : Jquery

--- 20220902/js/script.js
+++ 20220902/js/script.js
@@ -23,13 +23,45 @@ $(function () {
     if (sct >= 2300) {
       $(".s4_cover div").addClass("active");
     }
+
+    if (sct >= $("article").eq(0).offset().top) {
+      $("nav ul li").removeClass("on");
+      $("nav ul li").eq(0).addClass("on");
+    }
+    if (sct >= $("article").eq(1).offset().top) {
+      $("nav ul li").removeClass("on");
+      $("nav ul li").eq(1).addClass("on");
+    }
+    if (sct >= $("article").eq(2).offset().top) {
+      $("nav ul li").removeClass("on");
+      $("nav ul li").eq(2).addClass("on");
+    }
+    if (sct >= $("article").eq(3).offset().top) {
+      $("nav ul li").removeClass("on");
+      $("nav ul li").eq(3).addClass("on");
+    }
   });
   $("nav ul li").click(function () {
     var i = $(this).index(); // 클릭한 메뉴의 인덱스 번호를 구한다
     var offset_t = $("section > article").eq(i).offset().top;
     // alert(offset_t);
     $("html, body").stop().animate({ scrollTop: offset_t }, 1300);
-
+    // $("nav ul li").removeClass("on");
+    // $("nav ul li").eq(i).addClass("on");
     return false; //a 링크 금지
+  });
+
+  $("article").mousewheel(function (event, d) {
+    console.log(d);
+    if (d > 0) {
+      //휠을 위로 올릴 때
+      var prev = $(this).prev().offset().top;
+      $("html,body").stop().animate({ scrollTop: prev }, 1000, "easeOutBounce");
+    }
+    if (d < 0) {
+      //휠을 아래로 내릴 때
+      var next = $(this).next().offset().top;
+      $("html,body").stop().animate({ scrollTop: next }, 1000, "easeOutBounce");
+    }
   });
 });
