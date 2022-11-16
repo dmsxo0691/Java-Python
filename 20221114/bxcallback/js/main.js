0cd5398dbde55d2ed5dc5447bc5a8f0e0c65a979
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-11-16 14:00:41 +0900

    Html / bar

--- 20221114/bxcallback/js/main.js
+++ 20221114/bxcallback/js/main.js
@@ -1,6 +1,41 @@
 $(function () {
-  $(".slider").bxSlider({
+  var slider = $(".slider").bxSlider({
     auto: true,
     controls: false,
+    autoHover: true,
+    InfiniteLoop: true,
+    onSlideBefore: function () {
+      var n = slider.getCurrentSlide(); // 현재 슬라이드
+      var w = $("#page ul li").width(); // 넓이를 구해서 w대입
+      $("#page ul li").removeClass("on");
+      $("#page ul li").eq(n).addClass("on");
+
+      if (n == 0) {
+        $("#focus").css("left", "0");
+      } else {
+        $("#focus")
+          .stop()
+          .animate({ left: n * w }, 600);
+      }
+    },
+    onSlideAfter: function () {
+      var k = slider.getCurrentSlide();
+      $(".slider li").find("h1").removeClass("on");
+      $(".slider li .text" + k).addClass("on");
+    },
+  });
+
+  $(".left_btn").click(function () {
+    slider.goToPrevSlide();
+    return false;
+  });
+  $(".right_btn").click(function () {
+    slider.goToNextSlide();
+    return false;
+  });
+  $("#page ul li").click(function () {
+    var j = $(this).index();
+    slider.goToSlide(j);
+    return false;
   });
 });
